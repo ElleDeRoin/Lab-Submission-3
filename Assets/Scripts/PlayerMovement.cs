941486a64ca4bf86b2c1ba243a75012e9731e5f2
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2026-09-16 17:56:26 -0400

    ClampPlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public float speed = 5f;
+   public float minX = -10.25f;
+   public float maxX = 10.25f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(horizontal, 0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector2(clampedX, transform.position.y);
     }
 }
